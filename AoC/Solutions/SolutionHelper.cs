4c696af7c7440a60fa0645d33048cd0ed59409f1
siarhiej-h <light.drow.777@gmail.com>
2022-12-12 22:19:37 +0100

    Fixed build warnings.

---
+++ AoC/Solutions/SolutionHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace AoC.Solutions
+{
+    public static class SolutionHelper
+    {
+        public static (string first, string second) GetResults(this ISolution solution, InputModel inputModel)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(inputModel.Input);
+            string[] lines = inputModel.Input.Split("\n");
+
+            var firstResult = solution.CalculateFirstTask(lines);
+            var secondResult = solution.CalculateSecondTask(lines);
+            return (firstResult, secondResult);
+        }
+    }
+}
